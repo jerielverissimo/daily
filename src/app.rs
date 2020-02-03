e8b1cd9ed25c71fc0d5b52d36174c2c0b55187ac
Jeriel Baptista Verissimo <jeriel@ciandt.com>
2020-02-02 21:21:42 -0300

    Coupling app unit tests

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 enum InputMode {
     Normal,
     Insert,
@@ -19,5 +20,30 @@ impl<'a> App<'a> {
             input_mode: InputMode::Normal,
             tasks: Vec::new(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_create_a_new_app_with_title() {
+        // Arrange
+        let title = "Title Test";
+        // Act
+        let app = App::new(title);
+        // Assert
+        assert_eq!(app.title, title);
+    }
+
+    #[test]
+    fn should_create_a_new_app_with_default_input_mode() {
+        // Arrange
+        let title = "Title Test";
+        // Act
+        let app = App::new(title);
+        // Assert
+        assert_eq!(app.input_mode, InputMode::Normal);
     }
 }
